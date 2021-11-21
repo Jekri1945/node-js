195166363ced547db4db2a4ab1c2e622241bc282
Jekri194 <farisqi629@gmail.com>
2021-11-21 12:13:13 +0700

    initial commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
 // app.use('/js', express.static(__dirname + '/public/js'));
-// app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/images', express.static(__dirname + '/public/images'));
  
 var server = app.listen(8081, function(){
     var port = server.address().port;
